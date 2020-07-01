e182dc8eedd8fd903ac800a7fc9d4ebc99bb13e1
frvidal <frederic.vidal.perso@gmail.com>
2020-07-01 22:50:42 +0200

    Update app.module.ts

--- projects/test-pie-chart/src/app/app.module.ts
+++ projects/test-pie-chart/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { DynamicPieChartModule } from '@fitzhi/dynamic-pie-chart';
+import { DynamicPieChartModule } from 'dynamic-pie-chart';
 
 
 @NgModule({
